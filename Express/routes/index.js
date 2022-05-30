d58b1f826862d51c7d2072ba8bb241872c9adb0b
Teddy Dee <tkzteddy@gmail.com>
2022-05-30 15:41:11 +0700

    init Product

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 const invoice = require('./invoiceRouter')
+const product = require('./productRouter')
 
 router.use('/invoice', invoice)
+router.use('/product', product)
 
 module.exports = router
---
+++ Express/routes/productRouter.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const { } = require('../controllers/productController')
+
+const router = express.Router()
